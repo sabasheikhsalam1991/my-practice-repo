2cb46b478ed0cd397211bfafab7a31fdee4eecc1
sabasheikhsalam1991 <sabasheikhsalam1995@gmail.com>
2025-08-04 15:01:51 +0500

    adding field

--- practice/testing.cs
+++ practice/testing.cs
@@ -2,4 +2,5 @@ public class Testing
 {
     public string ID {  get; set; }=string.Empty;
     public string Name { get; set; } = string.Empty;
+    public string Name1 { get; set; } = string.Empty;
 }
