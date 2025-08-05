810c8499fdf8febfd9d656450b48165b8de6226d
sabasheikhsalam1991 <sabasheikhsalam1995@gmail.com>
2025-08-05 13:59:10 +0500

    testing

--- practice/testing.cs
+++ practice/testing.cs
@@ -3,4 +3,6 @@ public class Testing
     public string ID {  get; set; }=string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Name1 { get; set; } = string.Empty;
+    public string Name4 { get; set; } = string.Empty;
+    public string Name5 { get; set; } = string.Empty;
 }
