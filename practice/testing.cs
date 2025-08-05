c43fc39ae9b75e9d8e45669a70039b131b9544e6
sabasheikhsalam1991 <sabasheikhsalam1995@gmail.com>
2025-08-05 14:59:33 +0500

    add one more field with new user

--- practice/testing.cs
+++ practice/testing.cs
@@ -6,4 +6,5 @@ public class Testing
     public string Name4 { get; set; } = string.Empty;
     public string Name5 { get; set; } = string.Empty;
     public string Name6 { get; set; } = string.Empty;
+    public string Name7 { get; set; } = string.Empty;
 }
