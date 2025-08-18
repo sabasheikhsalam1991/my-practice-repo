ddd89384e3b3392ec2eb92fbb62fbbd6db01874c
Shaheryar Ali <43118465+Shahrey@users.noreply.github.com>
2025-08-18 14:45:01 +0500

    Update testing.cs

--- practice/testing.cs
+++ practice/testing.cs
@@ -1,6 +1,7 @@
 public class Testing
 {
     public string ID {  get; set; }=string.Empty;
+    public string IDs {  get; set; }=string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Name1 { get; set; } = string.Empty; 
     public string Name2 { get; set; } = string.Empty;
